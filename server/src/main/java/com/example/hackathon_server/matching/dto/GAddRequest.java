0156149498b905467fc0d6263f8e35bb0ccb40dc
choi-jaewon <sodlapdlf13@gmail.com>
2021-07-23 08:31:53 +0900

    add Gadd

---
+++ server/src/main/java/com/example/hackathon_server/matching/dto/GAddRequest.java
@@ -0,0 +1,51 @@
+package com.example.hackathon_server.matching.dto;
+
+import com.example.hackathon_server.matching.domain.MatchingType;
+import com.example.hackathon_server.user.domain.Company;
+import com.example.hackathon_server.user.domain.Gender;
+import com.example.hackathon_server.user.domain.Major;
+import com.example.hackathon_server.user.domain.UserType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import java.util.Map;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class GAddRequest {
+    String id = UUID.randomUUID().toString();
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 8)
+    private UserType userType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 8)
+    private MatchingType matchingType;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Major major;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 9)
+    private Company company;
+
+    public GAddRequest(Map map){
+        this.email = map.get("email").toString();
+        this.userType = UserType.valueOf(map.get("userType").toString());
+        this.matchingType = MatchingType.valueOf((map.get("matchingType").toString()));
+        this.company = Company.valueOf(map.get("company").toString());
+    }
+}
