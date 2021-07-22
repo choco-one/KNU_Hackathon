21f22ac308fc12ef63e589df828f0687eae81000
Go-Jaecheol <gojaech@naver.com>
2021-07-23 00:24:16 +0900

    Add CardView

--- client/app/src/main/java/com/example/hackathon_client/MainActivity.java
+++ client/app/src/main/java/com/example/hackathon_client/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             backpressedTime = System.currentTimeMillis();
             Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
         } else if (System.currentTimeMillis() <= backpressedTime + 2000) {
-            finish();
+            finishAffinity();
         }
     }
 
