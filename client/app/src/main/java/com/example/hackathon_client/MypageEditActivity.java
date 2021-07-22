6c1004a3a3aaf1109626959dd529e7628ffb6257
YunaSong97 <unme97@naver.com>
2021-07-22 17:10:24 +0900

    edit mypage -ing

--- client/app/src/main/java/com/example/hackathon_client/MypageEditActivity.java
+++ client/app/src/main/java/com/example/hackathon_client/MypageEditActivity.java
@@ -3,6 +3,7 @@ package com.example.hackathon_client;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +19,10 @@ public class MypageEditActivity extends AppCompatActivity {
     private TextView user_gender;
 
     private EditText user_phone;
-    private EditText user_major;
-    private EditText user_stdnum;
+    private RadioGroup major;
+    private RadioGroup type;
     private EditText user_career;
     private EditText user_interest;
-    private EditText user_type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +33,16 @@ public class MypageEditActivity extends AppCompatActivity {
         User user = (User) intent.getSerializableExtra("user");
 
         user_phone = findViewById(R.id.user_phone);
-        user_major = findViewById(R.id.user_major);
-        user_stdnum = findViewById(R.id.user_stdnum);
         user_career = findViewById(R.id.user_career);
         user_interest = findViewById(R.id.user_interest);
-        user_type = findViewById(R.id.user_type);
+        major = findViewById(R.id.major);
+        type = findViewById(R.id.type);
 
         user_phone.setHint(user.tel_number);
-        user_major.setHint(user.major);
-        user_stdnum.setHint(user.std_number);
         user_career.setHint(user.career);
         user_interest.setHint(user.interest);
-        user_type.setHint(user.userType);
+
+
     }
 
 
