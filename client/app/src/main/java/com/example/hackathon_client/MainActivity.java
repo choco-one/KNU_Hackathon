40455466231495a23b2caf50f766ae7d06a741b3
DDAGUE <DDAGUE@users.noreply.github.com>
2021-07-23 11:49:12 +0900

    Update MainActivity.java

--- client/app/src/main/java/com/example/hackathon_client/MainActivity.java
+++ client/app/src/main/java/com/example/hackathon_client/MainActivity.java
@@ -22,6 +22,8 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
